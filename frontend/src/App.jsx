15bee2ecc649a06b0663edaca730b81fea49e237
Anasyaseen92 <anasy7148@gmail.com>
2025-07-17 02:56:06 +0500

    complete UI of login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {LoginPage} from './Routes.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<LoginPage/>}/> 
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
