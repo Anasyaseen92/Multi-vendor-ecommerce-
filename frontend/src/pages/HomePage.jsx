cf2962a030fd86514ba795b5208ac72c1f20e5bf
Anasyaseen92 <anasy7148@gmail.com>
2025-07-30 02:10:32 +0500

    working on home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Layout/Header";
+import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
+import Categories from "../components/Route/Categories/Categories.jsx";
+import Hero from "../components/Route/Hero/Hero.jsx"
 
 function HomePage() {
   return (
     <div className=" min-h-screen bg-slate-100">
-      <Header />
+      <Header activeHeading={1}/>
+      <Hero/>
+      <Categories/>
+      <BestDeals/>
     </div>
   );
 }
