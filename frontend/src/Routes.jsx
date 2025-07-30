57afa2358b94bb35f56b5bd07eeca86b4402706a
Anasyaseen92 <anasy7148@gmail.com>
2025-07-31 03:03:36 +0500

    ,,,,

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,5 +2,6 @@ import LoginPage from "./pages/LoginPage.jsx"
 import SignupPage from "./pages/SignupPage.jsx"
 import ActivationPage from "./pages/ActivationPage.jsx"
 import HomePage from "./pages/HomePage.jsx"
+import ProductsPage from "./pages/ProductsPage.jsx"
 
-export { LoginPage , SignupPage, ActivationPage, HomePage };
+export { LoginPage , SignupPage, ActivationPage, HomePage, ProductsPage };
